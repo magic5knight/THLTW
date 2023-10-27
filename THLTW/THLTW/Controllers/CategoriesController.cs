fd75551c450376183831a150d7056867bd8d03d5
magic5knight <tuantranphuong@gmail.com>
2023-10-27 17:44:47 +0700

    Tuan5
    Bài thực hành tuần 5

--- THLTW/THLTW/Controllers/CategoriesController.cs
+++ THLTW/THLTW/Controllers/CategoriesController.cs
@@ -35,5 +35,52 @@ namespace THLTW.Controllers
                 return Content("LỖI TẠO MỚI CATEGORY");
             }
         }
+        public ActionResult Details(int id)
+        {
+            var category = database.Categories.Where(c => c.Id == id).FirstOrDefault();
+            return View(category);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var category = database.Categories.Where(c => c.Id == id).FirstOrDefault();
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Category category)
+        {
+            database.Entry(category).State = System.Data.Entity.EntityState.Modified;
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var category = database.Categories.Where(c => c.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                var category = database.Categories.Where(c => c.Id == id).FirstOrDefault();
+                database.Categories.Remove(category);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("Không xóa được do có liên quan đến bảng khác");
+            }
+        }
     }
 }
