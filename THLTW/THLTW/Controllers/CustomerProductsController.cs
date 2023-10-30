9011dd523edf1f214f9e61cf90a72f993e8ebb45
magic5knight <tuantranphuong@gmail.com>
2023-10-30 17:32:56 +0700

    Tuan7
    Bài thực hành tuần 7

---
+++ THLTW/THLTW/Controllers/CustomerProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using THLTW.Models;
+
+namespace THLTW.Controllers
+{
+    public class CustomerProductsController : Controller
+    {
+        private DBSportStoreEntities db = new DBSportStoreEntities();
+        // GET: CustomerProducts
+        public ActionResult Index()
+        {
+            var products = db.Products;
+            return View(products.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        public ActionResult ProductForCategory(int id)
+        {
+            string CategoryId = id.ToString();
+            var products = db.Products.Where(p => p.Category.Contains(CategoryId));
+            return View(products);
+        }
+    }
+}
---
+++ THLTW/THLTW/Models/CartItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using THLTW.Models;
+
+namespace THLTW.Models
+{
+    public class CartItem
+    {
+        DBSportStoreEntities db = new DBSportStoreEntities();
+        public int ProductID { get; set; }
+        public string NamePro { get; set; }
+        public string ImagePro { get; set; }
+        public decimal Price { get; set; }
+        public int Number { get; set; }
+
+        public decimal FinalPrice()
+        {
+            return Number * Price;
+        }
+
+        public CartItem( int productID)
+        {
+            this.ProductID = productID;
+            var productDB = db.Products.Single(s => s.ProductID == this.ProductID);
+            this.NamePro = productDB.NamePro;
+            this.ImagePro = productDB.ImagePro;
+            this.Price = (decimal)productDB.Price;
+            this.Number = 1;
+        }
+    }
+}
